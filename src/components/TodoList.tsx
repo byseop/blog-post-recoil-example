53bbd9ecac855aa703716c2f1279ecad00c2e7eb
Youngseop Bae <byseop@gmail.com>
2023-02-17 17:20:34 +0900

    feat: todo 아이템 삭제, 토글 구현

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,22 @@
-import { useRecoilValue } from 'recoil';
+import {useRecoilValue, useSetRecoilState} from 'recoil';
 import todoListState from '../recoil/todoList';
 import TodoItemCreator from './TodoItemCreator';
 
 const TodoList: React.FC<{}> = () => {
   const todoList = useRecoilValue(todoListState);
+  const setTodoList = useSetRecoilState(todoListState);
+
+  const handleClickRemoveButton = (id: number) => {
+    removeItem(id)
+  }
+
+  const handleClickItem = (id: number) => {
+    setTodoList(prev => prev.map(todo => todo.id === id ? ({ ...todo, isComplete: !todo.isComplete }) : todo))
+  }
+
+  const removeItem = (id: number) => {
+    setTodoList(prev => prev.filter(todo => todo.id !== id))
+  }
 
   return (
     <>
@@ -12,11 +25,11 @@ const TodoList: React.FC<{}> = () => {
       </div>
       <div>
         {todoList.map(todo => (
-          <div key={todo.id} style={{ cursor: 'pointer' }}>
+          <div key={todo.id} style={{ cursor: 'pointer' }} onClick={() => handleClickItem(todo.id)}>
             <span>
               {todo.isComplete ? <del>{todo.text}</del> : <>{todo.text}</>}
             </span>
-            <span><button type="button">삭제</button></span>
+            <span><button type="button" onClick={() => handleClickRemoveButton(todo.id)}>삭제</button></span>
           </div>
         ))}
       </div>
